5eed780dda626c65380335fcef305d88bbf8a0ac
yangjun9095 <yang-joon.kim@login-01.clusternet>
2023-09-27 11:10:46 -0700

    added run_mFinder_GRN.py and updated network analysis notebooks

---
+++ scripts/extract_nodes_from_GRN_network_motifs.py
@@ -0,0 +1,24 @@
+# This script is from XX.ipynb notebook from the zebrahub_multiome_analysis github repo.
+# This script extracts the nodes (gene_names) corresponding to the network motifs from a GRN.
+
+# Inputs:
+# GRN
+# network_motif
+# 
+
+# Outputs:
+# dataframe: gene
+
+
+# NOTE: Run this script within the celloracle_env conda environment
+
+# Import libraries
+import os
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scanpy as sc
+import seaborn as sns
+
