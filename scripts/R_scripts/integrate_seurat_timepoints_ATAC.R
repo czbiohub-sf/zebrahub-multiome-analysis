85344b119f43c15ae5b24bd6e40d11dc04b86382
yangjun9095 <yang-joon.kim@login-01.clusternet>
2024-02-08 23:17:42 -0800

    updated ATAC objectg integration scripts

---
+++ scripts/R_scripts/integrate_seurat_timepoints_ATAC.R
@@ -0,0 +1,121 @@
+# ## Notebook to integrate multiome objects (Seurat/Signac)
+
+# - Last updated: 02/06/2024
+# - Author: Yang-Joon Kim
+
+# - Step 1. load the multiome objects from all timepoints (6 timepoints, 2 replicates from 15-somites)
+# - Step 2. re-process the ATAC objects (merging peaks, re-computing the count matrices, then re-computing the PCA/LSI/SVD, seurat integration).
+# - Step 3. integrate the RNA objects (seurat integration using rPCA or CCA)
+# - Step 4. (optional - another notebook) alignUMAP for individual timepoints
+
+# load the libraries
+suppressMessages(library(Seurat))
+suppressMessages(library(Signac))
+library(SeuratData)
+library(SeuratDisk)
+library(Matrix)
+
+# genome info
+library(GenomeInfoDb)
+library(ggplot2)
+library(patchwork)
+library(stringr)
+library(BSgenome.Drerio.UCSC.danRer11)
+
+print(R.version)
+print(packageVersion("Seurat"))
+
+# parallelization in Signac: https://stuartlab.org/signac/articles/future
+library(future)
+plan()
+
+plan("multicore", workers = 32)
+plan()
+
+# set the max memory size for the future
+options(future.globals.maxSize = 512 * 1024 ^ 3) # for 512 Gb RAM
+
+# # Step 1. Load the datasets
+# # Load all seurat objects (replace the filepaths if you use this for your own datasets)
+# TDR118 <- readRDS("/hpc/projects/data.science/yangjoon.kim/zebrahub_multiome/data/processed_data/01_Signac_processed/TDR118reseq/TDR118_processed.RDS")
+# TDR119 <- readRDS("/hpc/projects/data.science/yangjoon.kim/zebrahub_multiome/data/processed_data/01_Signac_processed/TDR119reseq/TDR119_processed.RDS")
+# TDR124 <- readRDS("/hpc/projects/data.science/yangjoon.kim/zebrahub_multiome/data/processed_data/01_Signac_processed/TDR124reseq/TDR124_processed.RDS")
+# TDR125 <- readRDS("/hpc/projects/data.science/yangjoon.kim/zebrahub_multiome/data/processed_data/01_Signac_processed/TDR125reseq/TDR125_processed.RDS")
+# TDR126 <- readRDS("/hpc/projects/data.science/yangjoon.kim/zebrahub_multiome/data/processed_data/01_Signac_processed/TDR126/TDR126_processed.RDS")
+# TDR127 <- readRDS("/hpc/projects/data.science/yangjoon.kim/zebrahub_multiome/data/processed_data/01_Signac_processed/TDR127/TDR127_processed.RDS")
+# TDR128 <- readRDS("/hpc/projects/data.science/yangjoon.kim/zebrahub_multiome/data/processed_data/01_Signac_processed/TDR128/TDR128_processed.RDS")
+
+# print("Loaded all the datasets")
+
+# # extract only "ATAC" modality for simpler processing
+# TDR118_ATAC <- TDR118[["peaks_merged"]]
+# TDR119_ATAC <- TDR119[["peaks_merged"]]
+# TDR124_ATAC <- TDR124[["peaks_merged"]]
+# TDR125_ATAC <- TDR125[["peaks_merged"]]
+# TDR126_ATAC <- TDR126[["peaks_merged"]]
+# TDR127_ATAC <- TDR127[["peaks_merged"]]
+# TDR128_ATAC <- TDR128[["peaks_merged"]]
+
+# # Step 2. Re-process the ATAC objects
+# # Step 2-1. creating a common peak set
+# combined.peaks <- UnifyPeaks(object.list = list(TDR118,TDR119,TDR124,
+#                                                 TDR125,TDR126,TDR127,
+#                                                 TDR128), mode = "reduce")
+# combined.peaks
+
+# print("peaks were combined/merged")
+
+# # Step 2-2. quantify peaks in each dataset (re-process the count matrices)
+# # define a function to quantify the peaks
+# recompute_count_matrices <- function(seurat_obj, combined.peaks, data_id){
+#     new.counts <- FeatureMatrix(
+#       fragments = Fragments(seurat_obj),
+#       features = combined.peaks,
+#       sep = c(":", "-"),
+#       cells = colnames(seurat_obj)
+#     )
+#     # seurat_obj[["peaks_integreated"]] <- CreateAssayObject(counts = new.counts)
+#     new_seurat_obj <- CreateSeuratObject(counts = new.counts, project = data_id,
+#                                          assay = "peaks_integrated")
+#     # add the data_id to the object
+#     new_seurat_obj$dataset <- data_id
+#     return(new_seurat_obj)
+# }
+
+# # recompute the count matrices for each dataset, save as "peaks_integrated" ChromatinAssay
+# TDR118_ATAC <- recompute_count_matrices(TDR118_ATAC, combined.peaks = combined.peaks, data_id="TDR118")
+# TDR119_ATAC <- recompute_count_matrices(TDR119_ATAC, combined.peaks = combined.peaks, data_id="TDR119")
+# TDR124_ATAC <- recompute_count_matrices(TDR124_ATAC, combined.peaks = combined.peaks, data_id="TDR124")
+# TDR125_ATAC <- recompute_count_matrices(TDR125_ATAC, combined.peaks = combined.peaks, data_id="TDR125")
+# TDR126_ATAC <- recompute_count_matrices(TDR126_ATAC, combined.peaks = combined.peaks, data_id="TDR126")
+# TDR127_ATAC <- recompute_count_matrices(TDR127_ATAC, combined.peaks = combined.peaks, data_id="TDR127")
+# TDR128_ATAC <- recompute_count_matrices(TDR128_ATAC, combined.peaks = combined.peaks, data_id="TDR128")
+
+# # merge all datasets, adding a cell ID to make sure cell names are unique
+# combined <- merge(x = TDR118_ATAC, 
+#                     y = list(TDR119_ATAC, TDR124_ATAC, TDR125_ATAC,
+#                             TDR126_ATAC, TDR127_ATAC, TDR128_ATAC), 
+#                             add.cell.ids = c("TDR118", "TDR119", "TDR124", "TDR125", "TDR126", "TDR127", "TDR128"))
+
+# saveRDS(combined, "/hpc/projects/data.science/yangjoon.kim/zebrahub_multiome/data/processed_data/01_Signac_processed/integrated_ATAC.RDS")  # save the combined object
+# print("Saved the combined object")
+
+# Loading the combined object (if we are resuming from here)
+combined <- readRDS("/hpc/projects/data.science/yangjoon.kim/zebrahub_multiome/data/processed_data/01_Signac_processed/integrated_ATAC.RDS")
+
+# make sure to change to the assay containing common peaks
+DefaultAssay(combined) <- "peaks_integrated"
+combined <- RunTFIDF(combined)
+combined <- FindTopFeatures(combined, min.cutoff = 20)
+combined <- RunSVD(
+  combined,
+  reduction.key = 'LSI_',
+  reduction.name = 'lsi',
+  irlba.work = 400
+)
+combined <- RunUMAP(combined, dims = 2:30, reduction = 'lsi')
+
+# DimPlot(combined, group.by = 'dataset', pt.size = 0.1)
+
+saveRDS(combined, "/hpc/projects/data.science/yangjoon.kim/zebrahub_multiome/data/processed_data/01_Signac_processed/integrated_ATAC.RDS")  # save the combined object
+print("Saved the combined object with LSI and UMAP")
