7c0886db76cadd2a570ff8242a9e07ad8ce286a5
yangjun9095 <yang-joon.kim@login-01.clusternet>
2024-08-02 10:10:24 -0700

    modified the python script for systematic in silico genetic perturbation (celloracle)

--- scripts/slurm_scripts/compute_co_in_silico_KO.sh
+++ scripts/slurm_scripts/compute_co_in_silico_KO.sh
@@ -10,9 +10,14 @@
 #SBATCH --mail-type=END,FAIL                  # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=yang-joon.kim@czbiohub.org  # Email to which notifications will be sent
 
+# # Usage check
+# if [ "$#" -ne 5 ]; then
+#     echo "Usage: sbatch $0 oracle_path data_id annotation figpath list_KO_genes [--use_pseudotime true/false] [--pseudotime_path path] [--systematic_KO true/false]"
+#     exit 1
+# fi
 # Usage check
-if [ "$#" -ne 5 ]; then
-    echo "Usage: sbatch $0 oracle_path data_id annotation figpath list_KO_genes"
+if [ "$#" -lt 5 ]; then
+    echo "Usage: sbatch $0 oracle_path data_id annotation figpath list_KO_genes [--use_pseudotime true/false] [--pseudotime_path path] [--systematic_KO true/false]"
     exit 1
 fi
 
@@ -22,6 +27,10 @@ fi
 # 3) annotation: annotation class (cell-type annotation)
 # 4) figpath: filepath for the figures
 # 5) list_KO_genes: a comma-separated list of KO genes
+# 6) (optional) --use_pseudotime: use different pseudotime method other than DPT (true/false)
+# 7) (optional) --pseudotime_path: pseudotime df filepath
+# 8) (optional) --systematic_KO: perform in silico KO for all TFs/genes (true/false)
+
 
 # Assign command-line arguments to variables
 # Define arguments for the first script
@@ -30,9 +39,36 @@ data_id=$2 # data_id
 annotation=$3 # annotation class
 figpath=$4 # output figure path
 list_KO_genes=$5 # a comma-separated list of KO genes
+shift 5
+
+# Additional optional arguments
+use_pseudotime=""
+pseudotime_path=""
+systematic_KO=""
+
+while (( "$#" )); do
+  case "$1" in
+    --use_pseudotime)
+      use_pseudotime="--use_pseudotime $2"
+      shift 2
+      ;;
+    --pseudotime_path)
+      pseudotime_path="--pseudotime_path $2"
+      shift 2
+      ;;
+    --systematic_KO)
+      systematic_KO="--systematic_KO $2"
+      shift 2
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
 
 # Create the output directory if it doesn't exist
-# mkdir -p "$input_path"
+mkdir -p "$figpath"
 
 # Load necessary modules
 module load anaconda
@@ -43,8 +79,8 @@ conda activate celloracle_env
 cd /hpc/projects/data.science/yangjoon.kim/zebrahub_multiome/zebrahub-multiome-analysis/scripts/
 
 
-# Run the Python script with command-line arguments
-python run_07_co_KO_simulation.py $oracle_path $data_id $annotation $figpath $list_KO_genes
+# Run the Python script with command-line arguments 
+python run_07_co_KO_simulation.py $oracle_path $data_id $annotation $figpath $list_KO_genes $use_pseudotime $pseudotime_path $systematic_KO
 
 # Deactivate the conda environment
 conda deactivate
