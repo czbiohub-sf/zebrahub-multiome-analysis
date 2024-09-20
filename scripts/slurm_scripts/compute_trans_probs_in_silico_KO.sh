277a5dc16f3853c5ec56fa647e0bb2e729002edb
yangjun9095 <yang-joon.kim@login-01.clusternet>
2024-09-20 15:27:57 -0700

    cleaning up the github repo

--- scripts/slurm_scripts/compute_trans_probs_in_silico_KO.sh
+++ scripts/slurm_scripts/compute_trans_probs_in_silico_KO.sh
@@ -6,26 +6,24 @@
 #SBATCH --error=/hpc/projects/data.science/yangjoon.kim/zebrahub_multiome/data/processed_data/slurm_logs/co_insilico_KO_%j.err  # STDERR file
 #SBATCH --time=24:00:00                 # Runtime in HH:MM:SS
 #SBATCH --mem=32G                       # Memory total in GB (for all cores)
-#SBATCH --cpus-per-task=4               # Number of CPU cores per task
+#SBATCH --cpus-per-task=8               # Number of CPU cores per task
 #SBATCH --mail-type=END,FAIL            # Type of email notification
 #SBATCH --mail-user=yang-joon.kim@czbiohub.org  # Email for notifications
 
 # Usage check
-if [ "$#" -lt 5 ]; then
-    echo "Usage: sbatch $0 adata_path oracle_path data_id figpath list_KO_genes"
+if [ "$#" -lt 3 ]; then
+    echo "Usage: sbatch $0 oracle_path data_id list_KO_genes"
     exit 1
 fi
 
 # Assign command-line arguments to variables
-adata_path=$1
 # metacell_path=$2
-oracle_path=$2
-data_id=$3
-figpath=$4
-list_KO_genes=$5
+oracle_path=$1
+data_id=$2
+list_KO_genes=$3
 
 # Create the output directory if it doesn't exist
-mkdir -p "$figpath"
+# mkdir -p "$figpath"
 
 # Load necessary modules
 module load anaconda
@@ -36,11 +34,9 @@ conda activate celloracle_env
 cd /hpc/projects/data.science/yangjoon.kim/zebrahub_multiome/zebrahub-multiome-analysis/scripts/
 
 # Run the Python script with command-line arguments 
-python run_07_co_in_silico_KO_systematic.py \
-    --adata_path "$adata_path" \
+python run_07_01_co_in_silico_KO_systematic_delta_embedding.py \
     --oracle_path "$oracle_path" \
     --data_id "$data_id" \
-    --figpath "$figpath" \
     --list_KO_genes "$list_KO_genes"
 
 # Deactivate the conda environment
